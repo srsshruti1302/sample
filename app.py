70c6b3ab80628b7de156c0017da0d5e81a2ed047
srsshruti1302 <srsshruti05@gmail.com>
2026-02-11 19:24:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
